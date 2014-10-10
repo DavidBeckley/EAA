68d64ecafd1c94e1183422db7e981b73a34f81a2
davidbeckley <david.beckley.x00080130@gmail.com>
2014-10-10 15:35:59 +0100

    Question 6 append from shell to text file

---
+++ Week4/diary.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#: Title : Question 6
+#: Date : 10/10/14
+#: Author : David Beckley
+#: Version : 1
+#: Description : using append operator >> add line to text file
+#: Options : 
+
+echo "Add text"
+read text
+echo $text >> diary.txt
+
+
+
