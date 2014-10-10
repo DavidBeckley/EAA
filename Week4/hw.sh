6cb3262dd701cc4c63de515f8e92b2af796d37a4
davidbeckley <david.beckley.x00080130@gmail.com>
2014-10-10 14:43:14 +0100

    question 3 Hello World

---
+++ Week4/hw.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#: Title : Q3 hello world
+#: Date : 10/10/14
+#: Author : David Beckley
+#: Version : 1
+#: Description : Question 3 Lab 3
+#: Options : 
+
+printf "%s\n" "Hello, World!" 
