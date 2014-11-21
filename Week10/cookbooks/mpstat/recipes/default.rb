6971ce0c763a2c2d3c85d84523b352c556f1f348
davidbeckley <david.beckley.x00080130@gmail.com>
2014-11-21 15:42:57 +0000

    Week10

---
+++ Week10/cookbooks/mpstat/recipes/default.rb
@@ -0,0 +1,24 @@
+#
+# Cookbook Name:: mpstat
+# Recipe:: default
+#
+# Copyright 2014, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+
+package "sysstat" do
+	action :install
+end
+
+user "sysmon" do
+	action :create
+	comment "sysmon"
+	home "/home/sysmon"
+	shell "/bin/bash"
+	supports :manage_home => true
+end
+
+file "/home/sysmon/config.dat" do
+	action :create
+end
---
+++ Week10/cookbooks/solo.rb
@@ -0,0 +1,2 @@
+cookbook_path ["/home/mint/EAA/Week10/cookbooks"]
+json_attibs "/home/mint/EAA/Week10/cookbooks/node.json"
